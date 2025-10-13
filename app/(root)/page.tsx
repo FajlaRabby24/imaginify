bbad7aff0eb72f69e17e8302af8fb4554e46a0b8
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-13 13:03:25 +0600

    install clerk and create signIn, signUp component and wrap whole app into clerkProvider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>Home</p>
+
+      <UserButton afterSwitchSessionUrl="/" />
+    </div>
+  );
 };
 
 export default Home;
