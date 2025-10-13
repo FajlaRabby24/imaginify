0ae7b61fe591d46ac0f760fa1ca1ada631d91bd8
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-13 13:24:53 +0600

    sidebar signedIn layout is done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
+import Sidebar from "@/components/shared/Sidebar";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
+      {/* <MobileNav />  */}
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
