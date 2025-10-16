aeb342f81e1632ac44e4b018af208e3f6f72095c
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-16 23:06:54 +0600

    Media Uploader design done in generative fiil section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import { ToastContainer } from "react-toastify";
 import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({
@@ -28,6 +29,7 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          <ToastContainer theme="dark" position="top-left" autoClose={1500} />
           {children}
         </body>
       </html>
